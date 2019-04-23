b9c93aea093f7695fb5554ce1b84f6db909d5e47
Jay Miller <kjaymiller@users.noreply.github.com>
2019-04-23 16:41:11 -0700

    update /conferences

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ def test(req, resp):
     resp.text = 'Hello from Conftalks'
 
 @api.route("/conferences/{conference_ID}")
-def conferences(req, resp):
+def conferences(req, resp, *, conference_ID):
     resp.text = 'Lookin\' for some conferences?'
 
 if __name__ == '__main__':
