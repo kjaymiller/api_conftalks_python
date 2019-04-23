14ff21ea7256ac2af7c24cbe17774a356d3a8201
Kevin Miller <kmiller@w99x788i.local>
2019-04-23 13:56:38 -0700

    add falcon run

--- api.py
+++ api.py
@@ -1,4 +1,9 @@
 import falcon
 
 class conferenceResource:
-    def on_get(self)
+    def on_get(self, req, resp):
+        resp.media = {'text': 'Hello from Conftalks'}
+
+api = falcon.API()
+api.add_route('/quote', conferenceResource())
+        
