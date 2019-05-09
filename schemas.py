6969f837c8e1762124805829f141bfe40d0a9b8b
Kevin Miller <kmiller@w99x788i.local>
2019-05-09 16:01:35 -0700

    split api file to make it easier to manage

---
+++ schemas.py
@@ -0,0 +1,31 @@
+from api import api
+from marshmallow import Schema, fields
+
+
+@api.schema('Event')
+class EventSchema(Schema):
+    id = fields.Str(attribute="_id.$oid")
+    conference = fields.Str()
+    url = fields.Str()
+    name = fields.Str()
+    start_date = fields.Str(attribute="start_date.$date")
+    start_reminders = fields.List(fields.String)
+    end_date = fields.Str(attribute="end_date.$date")
+    end_reminders = fields.List(fields.String)
+    subscribed = fields.Boolean()
+
+
+@api.schema('Conference')
+class ConferenceSchema(Schema):
+    id = fields.Str(attribute="_id.$oid")
+    url = fields.Str()
+    name = fields.Str()
+    subscribed = fields.Boolean()
+
+
+@api.schema('User')
+class UserSchema(Schema):
+    id = fields.Str(attribute="_id.$oid")
+    name = fields.Str()
+    subscribed_conferences = fields.List(fields.String)
+    subscribed_events = fields.List(fields.String)
