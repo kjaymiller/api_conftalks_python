80f4288b08adc7cb13e65af5f284639574185666
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-05-14 16:20:06 -0700

    patching in tests

--- tests/test_conferences.py
+++ tests/test_conferences.py
@@ -1,19 +1,23 @@
 import pytest
 import json
 import api as service
+from mongo import get_db_data
 
 @pytest.fixture
 def api():
     return service.api
 
-
-def test_get_one_conference(api):
+def test_get_one_conference(mocker, api):
     """TODO: Mock Database Connection"""
-    r = api.requests.get('/conferences/5cbfa011127c6adabe9bfcb3')
+    mocker.patch(
+            'mongo.get_db_data', 
+            print('hi!!')
+            )
+    r = api.requests.get('/conferences/8675309')
     assert r.json()['id'] == '5cbfa011127c6adabe9bfcb3'
 
 
-def test_get_all_conferences(api):
-    """TODO: Mock Database Connection"""
-    r = api.requests.get('/conferences')
-    assert r.json()[0]['id'] == '5cbfa011127c6adabe9bfcb3'
+#    def test_get_all_conferences(api):
+#        """TODO: Mock Database Connection"""
+#        r = api.requests.get('/conferences')
+#        assert r.json()[0]['id'] == '5cbfa011127c6adabe9bfcb3'
