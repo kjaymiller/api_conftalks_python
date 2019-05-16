4391e567087779adb22da2f3b45051c56d66be42
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-05-16 08:22:59 -0700

    patching in tests/test_conferences

--- tests/test_conferences.py
+++ tests/test_conferences.py
@@ -2,20 +2,50 @@ import pytest
 import api as service 
 import conferences
 import mongo
+from random import randint
+from faker import Faker
+
+
+fake = Faker()
+
 
 @pytest.fixture
 def api():
     return service.api
 
-def test_get_one_conference(api, mocker, faker):
-    """TODO: Mock Database Connection"""
-    name = faker.company() 
-    id = faker.password(length=13) 
-    mocker.patch.object('conferences.get_db_data', lambda:{'name': name, '_id': id})
-    r = api.requests.get('/conferences/5cbfa011127c6adabe9bfcb3')
+
+def gen_fake_conference():
+    name = fake.company() + 'Con' 
+    _id = fake.password(length=13, special_chars=False) 
+    return {'name': name, '_id': {'$oid':_id}}
 
 
-#    def test_get_all_conferences(api):
-#        """TODO: Mock Database Connection"""
-#        r = api.requests.get('/conferences')
-#        assert r.json()[0]['id'] == '5cbfa011127c6adabe9bfcb3'
+@pytest.fixture
+def mocked_db_get_many():
+    mocks = map(lambda x: gen_fake_conference(), range(randint(2,10)))
+    return list(mocks)
+
+     
+def test_get_one_conference(api, mocker):
+    fake_data = gen_fake_conference()
+    _id = fake_data['_id']['$oid']
+    name = fake_data['name']
+    
+    mocker.patch(
+        'conferences.get_db_data', 
+        lambda x, _id: fake_data, 
+        )
+    r = api.requests.get(f'/conferences/{_id}')
+    assert r.json()['id'] == _id
+    assert r.json()['name'] == name
+
+
+def test_get_all_conferences(api, mocker, mocked_db_get_many):
+    mocker.patch(
+            'conferences.get_db_data',
+            lambda x: mocked_db_get_many,
+            )
+
+    r = api.requests.get('/conferences')
+    assert r.json()[1]['id'] == mocked_db_get_many[1]['_id']['$oid']
+
