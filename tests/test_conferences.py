472cda14b1bc0a3fe9288b256eb841e891bee665
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-05-16 16:25:16 -0700

    add more test data

--- tests/test_conferences.py
+++ tests/test_conferences.py
@@ -2,8 +2,9 @@ import pytest
 import api as service 
 import conferences
 import mongo
-from random import randint
+from random import randint, choice
 from faker import Faker
+import maya
 
 
 fake = Faker()
@@ -14,28 +15,49 @@ def api():
     return service.api
 
 
-def gen_fake_conference():
+def gen_fake_conference_data():
     name = fake.company() + 'Con' 
-    _id = fake.password(length=13, special_chars=False) 
-    return {'name': name, '_id': {'$oid':_id}}
+    _id = {'$oid': fake.password(length=13, special_chars=False)}
+    fake_start_datetime = maya.when(
+            str(fake.future_date(end_date="+1y")),
+            timezone=fake.timezone())
+    fake_end_datetime = fake_start_datetime.add(days=randint(1,365))
 
+    event_data = {
+            'id': _id, 
+            'name': name,
+            'organizers': map(fake.company_email(), range(randint(0,5))),
+            'event_start': fake_start_datetime.rfc2822(),
+            'event_end': fake_end_datetime.rfc2822(),
+            }
+
+    return event_data
+
+@pytest.fixture()
+def fake_conference_data():
+    return gen_fake_conference_data()
 
 @pytest.fixture
 def mocked_db_get_many():
-    mocks = map(lambda x: gen_fake_conference(), range(randint(2,10)))
+    mocks = map(lambda x: gen_fake_conference_data(), range(randint(2,10)))
     return list(mocks)
 
+def mocked_db_post():
+    pass
+
+def mocked_db_update():
+    pass
      
-def test_get_one_conference(api, mocker):
-    fake_data = gen_fake_conference()
-    _id = fake_data['_id']['$oid']
-    name = fake_data['name']
+def test_get_one_conference(api, mocker, fake_conference_data):
+    _id = fake_conference_data['id']
+    name = fake_conference_data['name']
     
     mocker.patch(
         'conferences.get_db_data', 
-        lambda x, _id: fake_data, 
+        lambda x, _id: fake_conference_data, 
         )
     r = api.requests.get(f'/conferences/{_id}')
+    print(r.json())
     assert r.json()['id'] == _id
     assert r.json()['name'] == name
 
@@ -47,5 +69,5 @@ def test_get_all_conferences(api, mocker, mocked_db_get_many):
             )
 
     r = api.requests.get('/conferences')
-    assert r.json()[1]['id'] == mocked_db_get_many[1]['_id']['$oid']
-
+    for x, row in enumerate(r.json()):
+        assert row['id'] == mocked_db_get_many[x]['_id']
--- tests/test_events.py
+++ tests/test_events.py
@@ -20,3 +20,18 @@ def test_subscribe_to_event(api):
 
     r = api.requests.post('/event/5cd10cc1bfda05ee8db8f5d9/subscribe', headers=headers, json={'start': True, 'end': False})
     assert r.json()
+
+    """
+    event_types = (
+            'Call for Proposal Registration', 
+            'Workshop Registration',
+            'Tickets On Sale',
+            'Lodging Discount Window',
+            'Meetup RSVP',
+            'Meet & Greet RSVP!',
+            )
+
+    for event in event_types:
+        _id = fake.password(length=13, special_chars=False) 
+    mocker.path('conferences.load_db_data',
+    """
